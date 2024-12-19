c94a6f797adb7d9706792557b3846ce9fd178640
NiskashY <roman.pantus@mail.ru>
2024-12-20 01:17:14 +0300

    add lab 2

---
+++ lab_2/test_specific_shirt.js
@@ -0,0 +1,38 @@
+const shirt =
+{
+    "name": "Car",
+    "description": "As you move in to campus, one of the first memories so many students have is driving up to their dorm, unloading their bags, and moving in. How do they arrive to campus? By car, of course!",
+    "price": "$10.99",
+    "colors": [
+        {
+            "color": "white",
+            "front": "shirt_images/car-white-front.png",
+            "back": "shirt_images/car-white-back.png",
+        },
+        {
+            "color": "blue",
+            "front": "shirt_images/car-blue-front.png",
+            "back": "shirt_images/car-blue-back.png",
+        },
+        {
+            "color": "green",
+            "front": "shirt_images/car-green-front.png",
+            "back": "shirt_images/car-green-back.png",
+        },
+        {
+            "color": "yellow",
+            "front": "shirt_images/car-yellow-front.png",
+            "back": "shirt_images/car-yellow-back.png",
+        },
+        {
+            "color": "red",
+            "front": "shirt_images/car-red-front.png",
+            "back": "shirt_images/car-red-back.png"
+        },
+    ],
+    "default": {
+        "color": "default",
+        "front": "shirt_images/default-w-front.png",
+        "back": "shirt_images/default-w-back.png"
+    }
+};
